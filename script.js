192bc69381379d8217addfc1f367c9728cb31c55
Michoel Rivkin <michoelr111@gmail.com>
2022-12-28 09:45:33 -0500

    my first javascript update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement('h2');
-h2.textContent = 'this content added by javascript';
+h2.textContent = '';
 document.querySelector('body').appendChild(h2);
+const plzWork = document.getElementById('plzWork');
+function hoverMessage(e){
+    alert ('he will probably be MVP');
+    console.log(e)
+}
+plzWork.addEventListener( "click", hoverMessage)
