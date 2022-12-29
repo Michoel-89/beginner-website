959f4ae164bbe9c47d4cbcdf02449e9670fb044f
Michoel Rivkin <michoelr111@gmail.com>
2022-12-29 18:31:48 -0500

    button update

--- script.js
+++ script.js
@@ -6,5 +6,19 @@ function hoverMessage(e){
     alert ('he will probably be MVP');
     console.log(e)
 }
-plzWork.addEventListener( "click", hoverMessage)
+plzWork.addEventListener( "click", hoverMessage);
+const changeColor = document.getElementById('changeColor');
+function date(event){
+    event.target.style.color = "purple";
+};
+changeColor.addEventListener("mouseenter", date);
+function toggle(e){  
+    const experiment = document.getElementById('experiment')
+    if (experiment.style.display === "none") {
+        experiment.style.display = 'block';
+    } else {
+        experiment.style.display = 'none';
+    }
+}
+
 
