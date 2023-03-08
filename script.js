cf485ac9935b4a4a0b6e0068f000651680ce9d93
michoel <michoelr111@gmail.com>
2023-03-08 18:04:08 -0500

    updated id plzwork

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const h2 = document.createElement('h2');
 h2.textContent = '';
 document.querySelector('body').appendChild(h2);
-const plzWork = document.getElementById('plzWork');
+const alertMessage = document.getElementById('alertMessage');
 function hoverMessage(e){
     alert ('he will probably be MVP');
     console.log(e)
 }
-plzWork.addEventListener( "click", hoverMessage);
+alertMessage.addEventListener( "click", hoverMessage);
 const changeColor = document.getElementById('changeColor');
 function changeTheColor(event){
     event.target.style.color = "silver";
