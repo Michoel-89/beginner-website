08972b91fa54b42cd932ef8423d194f4b7ae9330
michoel <michoelr111@gmail.com>
2023-03-11 22:24:37 -0500

    about me update

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const h2 = document.createElement('h2');
 h2.textContent = '';
 document.querySelector('body').appendChild(h2);
 const alertMessage = document.getElementById('alertMessage');
-function hoverMessage(e){
+function hoverMessage(){
     alert ('he will probably be MVP');
-    console.log(e)
 }
 alertMessage.addEventListener( "click", hoverMessage);
 const changeColor = document.getElementById('changeColor');
@@ -12,6 +11,5 @@ function changeTheColor(event){
     event.target.style.color = "silver";
 };
 changeColor.addEventListener("mouseenter", changeTheColor);
-
 
 
